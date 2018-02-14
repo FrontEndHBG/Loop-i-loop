984f812fdcbc4b7a3735b3e5d2b9b0b4895fd2f0
Max Carlquist <tenkakle@gmail.com>
2018-02-14 15:26:40 +0100

    finished

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-var items = [];
+var shoppingList = ['Coffee', 'Milk', 'Sugar', 'Bananas', 'Oatmeal', 'Potatoes', 'Eggs'];
+var list = document.getElementById('list');
+
+for (var i = 0; i < shoppingList.length; i++) {
+    var itemInList = shoppingList[i];
+    var li = document.createElement('li');
+    li.innerHTML = itemInList;
+    list.appendChild(li);
+}   
